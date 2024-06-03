f0769a3f1519dfaa128574e80fb84e7f903c1e90
nischhal-nub <nischalbh51@gmail.com>
2024-06-03 16:04:30 +0545

    useRef and useScroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { motion, useMotionValue, useScroll, useSpring, useTransform } from 'framer-motion'
 import './App.css'
+import { useRef } from 'react'
 
 function App() {
   const x = useMotionValue(0)
   const opacity = useTransform(x, [-100, 0, 100], [0, 1, 0])
-  const {scrollYProgress} = useScroll()
+  const containerRef = useRef()
+  const {scrollYProgress} = useScroll({container:containerRef})
   const scaleX = useSpring(scrollYProgress,{
     stiffness:100,
     damping:10
   })
-  
   return (
     <>
-    <motion.div style={{scaleX}} className='fixed top-0 left-0 right-0 bg-fuchsia-900 h-5 transform origin-left'></motion.div>
       <motion.h1
         initial={{ y: "-100%" }}
         animate={{ y: 0 }}
@@ -32,9 +32,11 @@ function App() {
           dragConstraints={{left:0, right:2, top:2, bottom:2}}
         />
       </div>
-          <motion.div></motion.div>
-        <div className='w-[60%] mx-auto'>
-          {Array.from({length:10}).map((_,i)=>(
+    
+        <div className='w-[60%] mx-auto relative h-96 overflow-scroll mt-10' ref={containerRef}>
+          <motion.div style={{scaleX}} className='fixed op-2 left-0 right-0 bg-fuchsia-900 h-5 origin-left'></motion.div>
+
+          {Array.from({length:20}).map((_,i)=>(
             <p className='mt-2' key={i}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam, ratione asperiores. Obcaecati corporis ad vero! Quo animi dignissimos eaque aut! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, sed perferendis architecto accusantium earum consequuntur dolor! Odit placeat accusantium incidunt. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur, sunt?</p>
           ))}
           <p></p>
