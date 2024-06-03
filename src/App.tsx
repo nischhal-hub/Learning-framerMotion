2016f567319634ec6f1d8ad41dd2d04f3ff759ec
nischhal-nub <nischalbh51@gmail.com>
2024-06-03 15:11:47 +0545

    configure : framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
+import { motion, useMotionValue, useTransform } from 'framer-motion'
 import './App.css'
 
 function App() {
+  const x = useMotionValue(0)
+  const opacity = useTransform(x, [-100, 0, 100], [0, 1, 0])
   return (
     <>
-      <h1 className='text-center mt-4 text-3xl font-bold'>Learning Framer motion</h1>
-      
+      <motion.h1
+        initial={{ y: "-100%" }}
+        animate={{ y: 0 }}
+        transition={{ duration: .4 }}
+        className='text-center mt-6 text-3xl font-bold'>Learning Framer motion</motion.h1>
+      <div className='w-full flex flex-col justify-center items-center mt-4'>
+        <p>Drag the box</p>
+        <motion.div
+          style={{
+            x,
+            opacity,
+            height: 100,
+            width: 100,
+            background: 'red'
+          }}
+          drag = 'x'
+          dragConstraints={{left:0, right:2, top:2, bottom:2}}
+        />
+      </div>
+
     </>
+
   )
 }
 
