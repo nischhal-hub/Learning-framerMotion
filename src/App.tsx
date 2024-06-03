549d17bf16dc1a1a6db9d99ed7f52c2582e6814f
nischhal-nub <nischalbh51@gmail.com>
2024-06-03 14:08:13 +0545

    init: project and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-center mt-4 text-3xl font-bold'>Learning Framer motion</h1>
+      
+    </>
+  )
+}
+
+export default App
